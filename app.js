133769677662b9eb52d5b18a06ac370e21e998a5
chry902 <tchry902@gmail.com>
2021-11-02 17:22:48 +0100

    reazione struttura

--- app.js
+++ app.js
@@ -3,14 +3,17 @@
 
 /**Rifare l'esercizio della to do list come fatto assieme in classe:
 stampare in pagina un item per ogni elemento contenuto in un array
-ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
-predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista */
+ogni item ha una "x" associata: cliccando su di essa, l'item viene 
+rimosso dalla lista
+predisporre un input per aggiungere un nuovo item alla lista: digitando 
+il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista */
 
 
 
 const app = new Vue({
     el: `#app`,
     data: {
+        image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfcOlzUYGV7KlCyvQ7qIjuQMsaK-s5LObEkzGO0lh5lqwEQKjITE_NqB_cyUtU0at5cw&usqp=CAU`,
         //nuva task
         newTask: ``,
         tasks: [
